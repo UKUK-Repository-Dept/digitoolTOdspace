53558c97914493dfe71f780de4aefd6a3748537f
Jitka Novotná <jitka@ucw.cz>
2019-08-01 15:54:12 +0200

    refoctoring categorize

--- source/categorize.py
+++ source/categorize.py
@@ -17,45 +17,29 @@ class Categorize():
         assert export in ['no','list','id_on_row','with_reason']
         self.export = export
 
-    def categorize_list(self, oai_ids, descriptions):
-        for oai_id in oai_ids:
-            self.categorize_ingest(oai_id, descriptions)
-    
     def categorize_item(self, oai_id, description):
         self.__categorize_ingest(oai_id, description)
     
     def __categorize_ingest(self, oai_id, description):
-        if  self.dtx.get_category(oai_id):
-            label, ingest, note = self.dtx.get_category(oai_id)
-        else:
-            return
-        for tag in self.ingests:
-            if ingest != None and tag in ingest:
-                self.category[tag].setdefault(oai_id,[]).append(description)
+        label, ingest, note = self.dtx.get_category(oai_id)
         if ingest == None:
             self.__categorize_note(oai_id, description, note)
         else:
-            other = True
-            for tag in self.ingests:
-                if tag in ingest:
-                    other = False
-            if other:
+            tags = [tag for tag in self.ingests if tag in ingest]
+            if tags == []:
                 self.category['other ingest'].setdefault(oai_id,[]).append(description)
+            else:
+                self.category[tags[0]].setdefault(oai_id,[]).append(description)
     
     def __categorize_note(self, oai_id, description, note):
-        for tags in self.notes:
-            if note != None and note in tags:
-                self.category[str(tags)].setdefault(oai_id,[]).append(description)
         if note == None:
             self.category['None note'].setdefault(oai_id,[]).append(description)
         else:
-            other = True
-            for tags in self.notes:
-                for tag in tags:
-                    if tag == note:
-                        other = False
-            if other:
+            tags = [tags for tags in self.notes if note in tags]
+            if tags == []:
                 self.category['other note'].setdefault(oai_id,[]).append(description)
+            else:
+                self.category[str(tags[0])].setdefault(oai_id,[]).append(description)
 
     def __str__(self):
         sum = 0
